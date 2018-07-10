dd4c5803c71ac06d9376f7e926c428322e6490e6
Akira Ota 10nin <akira19870221@gmail.com>
2018-07-10 20:27:30 +0900

    Add get-api-version function.

--- src/conoha_msg/core.clj
+++ src/conoha_msg/core.clj
@@ -3,8 +3,13 @@
             [clojure.java.io :as io])
   (:import java.net.URL))
 
+(def account-entry-point "https://account.tyo1.conoha.io")
+
 (defn get-json [address]
   "get json from address (address is direct link to json)"
   (let [url (URL. address)]
     (with-open [r (io/reader (.openStream url))]
       (json/read r :key-fn keyword))))
+
+(defn get-api-version []
+  (:id (first (:versions (get-json account-entry-point)))))
