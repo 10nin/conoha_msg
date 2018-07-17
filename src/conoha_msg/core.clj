8025cf4dbeeae24cd9f47d3ee8ace840c8f12220
Akira Ota 10nin <akira19870221@gmail.com>
2018-07-17 22:48:01 +0900

    Add get token body from server function.

--- src/conoha_msg/core.clj
+++ src/conoha_msg/core.clj
@@ -4,18 +4,12 @@
             [clj-http.client :as client])
   (:import java.net.URL))
 
-(defn get-json [address]
-  "get json from address (address is direct link to json)"
-  (let [url (URL. address)]
-    (with-open [r (io/reader (.openStream url))]
-      (json/read r :key-fn keyword))))
-
-(defn post-json [address]
-  "post json to address."
-  (let [url (URL. address)]
-    ))
 (defn get-identity-version []
   (client/get "https://identity.tyo1.conoha.io/"))
 
-(defn get-token [user pass tenantid]
-  )
+(defn get-token-body [user pass tenantid]
+  (let [req (format "{\"auth\": {\"passwordCredentials\": {\"username\": \"%s\", \"password\": \"%s\"}, \"tenantId\": \"%s\"}}" user pass tenantid)]
+    (print req)
+    (client/post "https://identity.tyo1.conoha.io/v2.0/tokens"
+                 {:body req
+                  :accept "application/json"})))
