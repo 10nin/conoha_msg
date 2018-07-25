0366bde76b01ced9f5ea7731a2c0f5061fb0a8f6
Akira Ota 10nin <akira19870221@gmail.com>
2018-07-25 12:58:02 +0900

    Add parameter to get request function, and add get notification function.

--- src/conoha_msg/core.clj
+++ src/conoha_msg/core.clj
@@ -17,10 +17,21 @@
   (let [json-str (get-token-body user pass tenantid)]
     (:id (:token (:access (json/read-str (:body json-str) :key-fn keyword))))))
 
-(defn get-request [url token]
-  (client/get url
-              {:headers {"Accept" "application/json"
-                         "X-Auth-Token" token}}))
-
+(defn get-request
+  ([url token]
+   (get-request url token nil))
+  ([url token query]
+   (client/get url
+               {:headers {"Accept" "application/json"
+                          "X-Auth-Token" token}
+                :query-params query})))
+ 
 (defn get-security-group [token]
   (get-request "https://networking.tyo1.conoha.io/v2.0/security-groups" token))
+
+(defn get-notification
+  ([tenantid token offset limit]
+   (let [uri (format "https://account.tyo1.conoha.io/v1/%s/notifications" tenantid)]
+     (get-request uri token {"offset" offset, "limit" limit})))
+  ([tenantid token]
+   (get-notification tenantid token 0 1000)))
